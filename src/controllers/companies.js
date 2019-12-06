f1b63479dd2286299c33b0942fd96a981887c5e3
biFebriansyah <bukanebi@gmail.com>
2019-12-07 05:11:22 +0700

    add photo company

--- src/controllers/companies.js
+++ src/controllers/companies.js
@@ -1,7 +1,8 @@
 const models = require('../models/companies');
 const respon = require('../helpers/respon');
 const validate = require('../helpers/validate');
-
+const upload = require('../helpers/upload');
+const pathFIle = require('path');
 const valid = new validate();
 const model = new models();
 
@@ -19,22 +20,34 @@ module.exports = {
 
     add: async (req, res) => {
 
-        const data = {
-            name: req.body.name,
-            logo: req.body.logo,
-            location: req.body.location,
-            description: req.body.description,
-        };
-
-        const {error} = valid.validateCompany(data);
-
-        if (error) {
-            return respon(res, 400, error.details[0].message);
+        if (!req.files) {
+            return respon(res, 400, "Photo required");
         }
+        let photo = ''
+
+        if (req.files.photo.name) {
+            photo = req.files.photo
+        } else {
+            photo = req.files.photo[0]
+        }
+        const extension = pathFIle.extname(photo.name)
+        const path = process.cwd() + '\\src\\upload\\image\\' + 'Photo-'+ Date.now()  + extension;
+        await photo.mv(path)
+        const url = await upload(path);
+
+
+        const data = {
+            username: req.body.username,
+            name: req.body.name,
+            logo: url,
+            location: req.body.location,
+            email: req.body.email,
+            telp: req.body.telp,
+        };
 
         try {
             const result = await model.add(data);
-            return respon(res, 200, result);
+            return respon(res, 200, 'Success');
 
         } catch (error) {
             return respon(res, 500, error);
@@ -77,5 +90,18 @@ module.exports = {
         } catch (error) {
             return respon(res, 500, error);
         }
-    }
+    },
+
+    getDataBy: async (req, res) => {
+
+        const user = req.params.username;
+
+        try {
+            let result = await model.getDataBy(user);
+            return respon(res, 200, result);
+    
+        } catch (error) {
+            return respon(res, 500, error);
+        }
+    },
 }
